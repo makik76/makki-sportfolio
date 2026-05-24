a01d49eb0f4574e9ce45bad3cedcab5953c06f2e
Muhammad Makki <khanmaki9932@gmail.com>
2026-05-25 00:10:29 +0500

    Add Vercel Postgres support

--- makkisportfolio/settings.py
+++ makkisportfolio/settings.py
@@ -77,15 +77,25 @@ TEMPLATES = [
 WSGI_APPLICATION = "makkisportfolio.wsgi.application"
 
 
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.getenv("POSTGRES_URL") or os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.getenv("POSTGRES_URL", os.getenv("DATABASE_URL")),
+            conn_max_age=600,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
