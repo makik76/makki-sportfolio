231bdd602b4874713c16ca0b1a68014b696f66b9
Muhammad Makki <khanmaki9932@gmail.com>
2026-05-24 17:51:59 +0500

    Fix ALLOWED_HOSTS for Vercel

--- makkisportfolio/settings.py
+++ makkisportfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-fallback-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ".vercel.app,.onrender.com,localhost,127.0.0.1").split(",")
 
 
 # Application definition
